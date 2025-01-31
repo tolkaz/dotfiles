4881884e2fe025f0474683ece5d33b76fc87e4eb
tolkaz <spyci77@hotmail.com>
2025-01-31 02:31:20 -0500

    niri dot

--- .config/Thunar/accels.scm
+++ .config/Thunar/accels.scm
@@ -5,6 +5,7 @@
 ; (gtk_accel_path "<Actions>/ThunarStandardView/sort-by-type" "")
 ; (gtk_accel_path "<Actions>/ThunarStatusBar/toggle-last-modified" "")
 ; (gtk_accel_path "<Actions>/Thunarwindow/menu" "")
+; (gtk_accel_path "<Actions>/ThunarBookmarks/323b2032d33a95d6eed701fc586af0ba" "")
 ; (gtk_accel_path "<Actions>/ThunarActionManager/cut" "<Primary>x")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/sort-by-size" "")
 ; (gtk_accel_path "<Actions>/ThunarWindow/file-menu" "")
